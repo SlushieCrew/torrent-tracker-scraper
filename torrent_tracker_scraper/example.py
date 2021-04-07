c1c26dcaa69f5897ff0d12efe204e78549589cdb
Bendik Dyrli <bendik.dyrli@uia.no>
2021-04-07 11:19:24 +0000

    wtf !
    Signed-off-by: Bendik Dyrli <bendik.dyrli@uia.no>

--- torrent_tracker_scraper/example.py
+++ torrent_tracker_scraper/example.py
@@ -1,11 +1,24 @@
-if __name__ == "__main__":
-    import scraper
+# if __name__ == "__main__":
+# import scraper
 
-    scraper = scraper.Scraper(
-        trackers=["udp://bt2.archive.org:6969/annouce"],
-        infohashes=["73C36F980F5B1A40348678036575CCC1E0BB0E4E",],
-        timeout=5,
-    )
-    results = scraper.scrape()
-    for result in results:
-        print(result)
+# scraper = scraper.Scraper(
+# trackers=["udp://bt2.archive.org:6969/annouce"],
+# infohashes=["73C36F980F5B1A40348678036575CCC1E0BB0E4E",],
+# timeout=5,
+# )
+# results = scraper.scrape()
+# for result in results:
+# print(result)
+
+import scraper
+import json
+
+scraper = scraper.Scraper(
+    infohashes=[
+        "9bb80eaa0c2d384013784a8e2117d235e2de1073",
+    ]
+)
+results = scraper.scrape()
+# for result in results:
+print (results)
+    # print(json.dumps(json.loads(results), indent=4, sort_keys=True))
